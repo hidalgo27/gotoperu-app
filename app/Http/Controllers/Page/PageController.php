4baa88f5c7016725068559587ae19de2f545957b
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2023-08-21 14:14:15 -0500

    Base de la aplicacion creada

--- app/Http/Controllers/Page/PageController.php
+++ app/Http/Controllers/Page/PageController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Page;
 
 use App\Http\Controllers\Controller;
+use App\Models\TCategoria;
 use App\Models\TDestino;
+use App\Models\THotelDestino;
+use App\Models\TPais;
 use App\Models\TPaquete;
+use App\Models\TTeam;
+use App\Models\TTestimonio;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -16,5 +21,26 @@ class PageController extends Controller
     public function destinations(){
         $destinos = TDestino::latest()->take(3)->get();
         return (compact('destinos'));
+    }
+    public function packages_detail(TPaquete $paquete) {
+        $testinomials = TTestimonio::all();
+        $testinomials_r = TTestimonio::inRandomOrder()->limit(1)->get();
+        $category = TCategoria::all();
+
+        $hoteles_destinos = THotelDestino::all();
+
+        $teams = TTeam::all();
+        $pais2 = TPais::all();
+
+        return view('page.detail',
+            compact(
+                'paquete',
+                'category',
+                'testinomials',
+                'hoteles_destinos',
+                'testinomials_r',
+                'teams',
+                'pais2'
+            ));
     }
 }
